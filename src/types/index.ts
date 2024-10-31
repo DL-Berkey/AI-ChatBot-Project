881b866c3fd0a566597a61b1527b0b3850bd83dc
dl <dlhomura@gmail.com>
2024-10-31 20:57:15 +0900

    Supabase SSR code 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,9 @@
 import OpenAI from "openai";
+import { Database } from "./supabase";
 
 export type OpenAI_Message = OpenAI.Chat.Completions.ChatCompletionMessageParam;
 
 export type OpenAI_Messages =
     OpenAI.Chat.Completions.ChatCompletionMessageParam[];
+
+export type RoomData = Database["public"]["Tables"]["ConversationRoom"];
